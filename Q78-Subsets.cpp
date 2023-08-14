a30635cdf9f29c2655ed573a448282f019a14a8e
Parth Shukla <97020896+parth9643@users.noreply.github.com>
2023-08-14 16:33:50 +0530

    https://leetcode.com/problems/subsets/

--- Q78-Subsets.cpp
+++ Q78-Subsets.cpp
@@ -1,5 +1,6 @@
 //here we are going to tell most optimised way 
-
+//2 solution to approach the problem
+//Solution 1 
 class Solution {
 public:
 
@@ -19,5 +20,26 @@ public:
         vector<int>ans1;
         temp(nums,0,ans,ans1);
         return ans;
+    }
+};
+
+
+//Solution 2  -> Bit Manipulation
+
+class Solution {
+public:
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        for(int mask=0;mask<(1<<nums.size());mask++){
+            vector<int>ans1;
+            for(int i=0;i<nums.size();i++){
+                if(mask&(1<<i)){
+                    ans1.push_back(nums[i]);
+                }
+            }
+        ans.push_back(ans1);
+        }
+    return ans;
     }
 };
